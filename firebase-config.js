4215d6ea4b7666c7a70f47efee79ac833c573023
HeiricFTN <heiric@gmail.com>
2025-03-03 07:52:25 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,17 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+import { 
+    getFirestore,
+    collection,
+    doc,
+    addDoc,
+    getDoc,
+    getDocs,
+    setDoc,
+    query,
+    where,
+    orderBy
+} from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
 
 // Your Firebase configuration
